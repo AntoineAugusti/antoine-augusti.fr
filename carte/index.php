7ebcb35c2c6e82f53c8f860877956fbbaf6aaf45
Antoine Augusti <antoine@augusti.fr>
2012-09-30 17:10:30 +0200

    Add a CSS sprite

--- carte/index.php
+++ carte/index.php
@@ -3,14 +3,14 @@
 <head>
 <meta charset="utf-8"/>
 <title>Carte de visite | Antoine Augusti</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="style.css"/>
 <meta name="description" content="Carte de visite d'Antoine Augusti"/>
 <meta name="keywords" content="'Antoine Augusti', Augusti, 'Pretty Web'"/>
 <meta name="author" content="Antoine Augusti"/>
 <meta name="revisit-after" content="2 days"/>
 <meta name="Robots" content="all"/>
-<script src="//code.jquery.com/jquery-latest.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<script src="//code.jquery.com/jquery-latest.min.js"></script>
 <link rel="shortcut icon" type="image/x-icon" href="http://www.antoine-augusti.fr/images/favicon.png">
 <script> 
 $(document).ready(function(){
@@ -39,16 +39,16 @@ var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga
 	<h5 class="margin_title">Étudiant à l'<a href="//www.insa-rouen.fr" title="INSA Rouen" target="_blank">INSA Rouen</a></h5>
 	<h5>Développeur chez <a href="//quantic-telecom.net" title="Quantic Télécom" target="_blank">Quantic Télécom</a></h5>
 	<br/>
-	<img src="images/skills.png" class="icone"/>HTML5, CSS3, PHP, SQL, AJAX, JS, SPIP, WordPress...<br>
+	<span class="icone skills"></span>HTML5, CSS3, PHP, SQL, AJAX, JS, SPIP, WordPress...<br>
 	<br/>
 	<div class="contact">
-		<img src="images/iphone.png" class="icone" alt="Icone de portable"/>+33 6 87 09 98 92<br>
-		<img src="images/email.png" class="icone" alt="Icone d'email"/>antoine@augusti.fr<br>
-		<img src="images/skype.png" class="icone" alt="Icone de Skype"/>AntoineAugusti<br>
-		<img src="images/website.png" class="icone" alt="Icone de site internet"/><a href="//www.antoine-augusti.fr" title="Site personnel" target="_blank">www.antoine-augusti.fr</a><br>
-		<img src="images/frog.png" class="icone" alt="Logo de Pretty Web"/><a href="//www.pretty-web.com" title="Pretty Web" target="_blank">www.pretty-web.com</a><br>
-		<img src="images/facebook.png" class="icone" alt="Icone de Facebook"/><a href="//www.facebook.com/AntoineAugusti" title="Facebook Antoine Augusti" target="_blank">www.facebook.com/AntoineAugusti</a><br>
-		<img src="images/twitter.png" class="icone" alt="Icone de Twitter"/><a href="//twitter.com/AntoineAugusti" title="Twitter Antoine Augusti" target="_blank">@AntoineAugusti</a><br>
+		<span class="icone iphone"></span>+33 6 87 09 98 92<br>
+		<span class="icone email"></span>antoine@augusti.fr<br>
+		<span class="icone skype"></span>AntoineAugusti<br>
+		<span class="icone website"></span><a href="//www.antoine-augusti.fr" title="Site personnel" target="_blank">www.antoine-augusti.fr</a><br>
+		<span class="icone frog"></span><a href="//www.pretty-web.com" title="Pretty Web" target="_blank">www.pretty-web.com</a><br>
+		<span class="icone facebook"></span><a href="//www.facebook.com/AntoineAugusti" title="Facebook Antoine Augusti" target="_blank">www.facebook.com/AntoineAugusti</a><br>
+		<span class="icone twitter"></span><a href="//twitter.com/AntoineAugusti" title="Twitter Antoine Augusti" target="_blank">@AntoineAugusti</a><br>
 	</div>
 </div>
 <div id="footer">
