8cfdc24599ef81b93e489275758b8aff21388045
Antoine Augusti <antoine@augusti.fr>
2013-02-17 19:44:21 +0100

    Indentation

--- index.php
+++ index.php
@@ -13,15 +13,15 @@ include "head.php";
 	        	<div class="corps-right">
 	        		<h1><span class="icon-corps blog"></span><?php echo $lang['index_derniers_articles']; ?></h1>
 					<div class="gray-post">
-					<ul class="inside-square">
-					<?php
-					$reponse = mysql_query("SELECT guid, post_title, post_date FROM wp_posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY id DESC LIMIT 0,5");
-					while ($donnees = mysql_fetch_array($reponse) )
-						{
-					?>
-						<li class="margin-bottom-30"><?php convertirDate($donnees['post_date']); ?> -<a href="<?php echo $donnees['guid']; ?>" target="_blank"> <?php echo htmlentities($donnees['post_title']); ?></a></li>	
-					<?php } ?>
-					</ul>
+						<ul class="inside-square">
+							<?php
+							$reponse = mysql_query("SELECT guid, post_title, post_date FROM wp_posts WHERE post_status = 'publish' AND post_type = 'post' ORDER BY id DESC LIMIT 0,5");
+							while ($donnees = mysql_fetch_array($reponse) )
+								{
+							?>
+								<li class="margin-bottom-30"><?php convertirDate($donnees['post_date']); ?> -<a href="<?php echo $donnees['guid']; ?>" target="_blank"> <?php echo htmlentities($donnees['post_title']); ?></a></li>
+							<?php } ?>
+						</ul>
 					</div>
 	        	</div>
 
