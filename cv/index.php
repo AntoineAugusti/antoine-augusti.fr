9dbb9b2bd447cd845a936d20365d623da53b645d
Antoine Augusti <antoine@augusti.fr>
2013-02-25 10:19:45 +0100

    Analytics

--- cv/index.php
+++ cv/index.php
@@ -34,16 +34,17 @@ require "../kernel/fonctions.php";
 		$("#footer").delay(2000).fadeIn(1500);
 	});
 	</script>
-	<script>
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-12045924-3']);
-	  _gaq.push(['_setDomainName', '.antoine-augusti.fr']);
-	  _gaq.push(['_trackPageview']);
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : '//www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
+	<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-12045924-3']);
+	_gaq.push(['_setDomainName', 'antoine-augusti.fr']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
 	</script>
 </head>
 <!--
