a2381f9892c2fcdea8b51878ef0a949c53a64442
Antoine Augusti <antoine@augusti.fr>
2014-07-31 12:12:31 +0200

    Update the favicon

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -5,7 +5,7 @@
 	<title>{{ $pageTitle or 'Antoine Augusti' }}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	{{ HTML::style('assets/css/styles.min.css') }}
-	<link rel="shortcut icon" href="/assets/images/favicon.png"/>
+	<link rel="shortcut icon" href="/assets/images/favicon.jpg"/>
 	<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
