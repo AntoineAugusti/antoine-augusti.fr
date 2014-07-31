fb6ab414e2782cd80a28062d73fcfd8d2796227d
Antoine Augusti <antoine@augusti.fr>
2014-07-31 15:48:22 +0200

    Add some text to the contact section and update the footer

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,13 +1,8 @@
 @extends('partials.page')
 
 @section('content')
-	<section id="intro">
-		{{ HTML::image("assets/images/me.jpg", "Antoine Augusti", ['class' => 'animated fadeInDown', 'height' => 160, 'width' => 160])}}
-		<h1 class="fadeInUp animated animate-delay">Hello World, I'm Antoine Augusti</h1>
-	</section>
-
 	<section class="container">
-		<h1>Cool facts about me</h1>
+		<h1 id="anchor-presentation">Cool facts about me</h1>
 
 		<div class="row">
 			<section class="card wow col-md-6 animated slideInLeft">
@@ -25,7 +20,7 @@
 			</section>
 		</div>
 
-		<h1>Projects</h1>
+		<h1 id="anchor-projects">Projects</h1>
 
 		<div class="row">
 			<section class="card wow col-md-6 animated slideInLeft">
@@ -57,6 +52,42 @@
 					</div>
 				</article>
 			</section>
+		</div>
+	</section>
+
+	<section id="contact" class="container animated wow zoomIn">
+		<h1 id="anchor-contact">Get in touch</h1>
+		<p class="text">
+			So you want to do a little spying before contacting me? Here you are!
+		</p>
+		<div class="row">
+			<div class="col-xs-6 col-sm-3 animated wow zoomIn" data-wow-delay="0.5s">
+				<a href="https://twitter.com/AntoineAugusti"><i class="fa fa-twitter"></i></a>
+				<a href="https://twitter.com/AntoineAugusti" class="username">@AntoineAugusti</a>
+			</div>
+			<div class="col-xs-6 col-sm-3 animated wow zoomIn" data-wow-delay="0.5s">
+				<a href="https://github.com/AntoineAugusti"><i class="fa fa-github"></i></a>
+				<a href="https://github.com/AntoineAugusti" class="username">AntoineAugusti</a>
+			</div>
+			<div class="col-xs-6 col-sm-3 animated wow zoomIn" data-wow-delay="0.5s">
+				<a href="http://linkedin.com/in/antoineaugusti"><i class="fa fa-linkedin"></i></a>
+				<a href="http://linkedin.com/in/antoineaugusti" class="username">antoineaugusti</a>
+			</div>
+			<div class="col-xs-6 col-sm-3 animated wow zoomIn" data-wow-delay="0.5s">
+				<i class="fa fa-envelope"></i>
+				<span class="username">antoine@augusti.fr</span>
+			</div>
+		</div>
+		<div class="text animated wow fadeIn" data-wow-delay="2s">
+			<p>
+				Trying to know me by clicking links is fine, but you know what's better?<span class="animated wow fadeIn" data-wow-delay="4.5s"> Sending me an e-mail.</span><span class="animated wow fadeIn" data-wow-delay="6s"> Yes, it involves <em>typing</em>.</span>
+			</p>
+			<p class="animated wow fadeIn" data-wow-delay="8s">
+				Yeah, I know, sometimes it is difficult.
+			</p>
+		</div>
+		<div id="mocking" class="text animated wow fadeIn" data-wow-delay="11s">
+			Nope, that's all, there is nothing coming next.
 		</div>
 	</section>
 	
