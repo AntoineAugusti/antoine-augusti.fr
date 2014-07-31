ddb860ad6b3989ecabd78aa8dce3b27568df3ebe
Antoine Augusti <antoine@augusti.fr>
2014-07-31 15:47:26 +0200

    Smooth scroll

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,1 +1,30 @@
 new WOW().init();
+
+$(document).ready(function() {
+	// Smooth scroll
+	$('a[href^=#]').bind("click", jump);
+
+	if (location.hash) {
+		setTimeout(function() {
+			$('html, body').scrollTop(0).show();
+			jump();
+		}, 0);
+	} else {
+		$('html, body').show();
+	}
+});
+
+var jump = function(e) {
+	if (e) {
+		e.preventDefault();
+		var target = $(this).attr("href");
+	} else {
+		var target = location.hash;
+	}
+
+	$('html,body').animate({
+		scrollTop: $(target).offset().top - 30
+	}, 2000, function() {
+		location.hash = target;
+	});
+}
