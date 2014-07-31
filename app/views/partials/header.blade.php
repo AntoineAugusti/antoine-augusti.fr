29589ce97418b9b1b8d6855727003ac52a2c0e38
Antoine Augusti <antoine@augusti.fr>
2014-07-31 15:47:49 +0200

    Add a menu

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -15,13 +15,22 @@
 	<![endif]-->
 </head>
 <body>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-12045924-3', 'auto');
+		ga('send', 'pageview');
+	</script>
 
-	ga('create', 'UA-12045924-3', 'auto');
-	ga('send', 'pageview');
-</script>
+	<header>
+		<nav class="animated animate-delay-big bounceInDown">
+			<a href="#anchor-presentation">About me</a>
+			<a href="#anchor-projects">Projects</a>
+			<a href="#anchor-contact">Contact me</a>
+		</nav>
+		{{ HTML::image("assets/images/me.jpg", "Antoine Augusti", ['class' => 'animated fadeInDown', 'height' => 160, 'width' => 160])}}
+		<h1 class="fadeInUp animated animate-delay">Hello World, I'm Antoine Augusti</h1>
+	</header>
