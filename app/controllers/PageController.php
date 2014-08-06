80d3c2cdc9ac410b5eeebce01a14ccc898ab2055
Antoine Augusti <antoine@augusti.fr>
2014-08-06 19:02:39 +0200

    Retrieve latest blog articles

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -4,6 +4,11 @@ class PageController extends BaseController {
 
 	public function home()
 	{
-		return View::make('pages.home');
+		
+		// Retrieve last blog posts
+		$feed = FeedReader::read('http://blog.antoine-augusti.fr/feed/');
+		$lastArticles = $feed->get_items(0, 5);
+
+		return View::make('pages.home', compact('lastArticles'));
 	}
 }
