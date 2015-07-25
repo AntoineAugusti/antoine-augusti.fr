495bac1713ea5829405e34b2db937b2582af1772
Antoine Augusti <antoine@augusti.fr>
2015-07-25 22:29:47 +0100

    Update header

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -4,12 +4,14 @@
 	<meta charset="UTF-8">
 	<title>{{ $pageTitle or 'Antoine Augusti' }}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Antoine Augusti, web developer, software engineering and data mining." />
+
+	<meta name="description" content="Antoine Augusti, software engineering, machine learning, distributed systems." />
+
 	<link href='https://fonts.googleapis.com/css?family=Libre+Baskerville:400,700' rel='stylesheet' type='text/css'>
 	<link media="all" type="text/css" rel="stylesheet" href="/assets/css/styles.min.css">
 	<link rel="shortcut icon" href="/assets/images/favicon.jpg"/>
 
-	<link href="https://plus.google.com/104395355791731974347/" rel="author" />
+	<link href="https://plus.google.com/+AntoineAugusti" rel="author" />
 
 	<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
@@ -32,6 +34,6 @@
 			<a href="#anchor-projects">Projects</a>
 			<a href="#anchor-contact">Contact me</a>
 		</nav>
-		{{ HTML::image("assets/images/me.jpg", "Antoine Augusti", ['class' => 'animated fadeInDown', 'height' => 160, 'width' => 160])}}
+		{{ HTML::image("assets/images/me.jpg", "Antoine Augusti", ['height' => 160, 'width' => 160])}}
 		<h1 class="fadeInUp animated animate-delay">Hello World, I'm Antoine Augusti</h1>
 	</header>
