c17ce59220d3d0f1192151a3f1da8084a70bd812
Antoine Augusti <antoine@augusti.fr>
2015-07-25 21:14:52 +0100

    Move from Guard to Gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var elixir = require('laravel-elixir');
+require('laravel-elixir-sass-compass');
+
+
+elixir(function(mix) {
+    mix.compass("screen.scss", "app/assets/css", {
+        config_file: "config.rb",
+        sass: "app/assets/sass",
+    })
+    .scripts([
+        "jquery-2.1.0.min.js",
+        "app.js"
+    ], "public/assets/js/scripts.min.js", "app/assets/js")
+    .styles([
+        "bootstrap.min.css",
+        "font-awesome.min.css",
+        "screen.css"
+    ], 'public/assets/css/styles.min.css', 'app/assets/css');
+});
