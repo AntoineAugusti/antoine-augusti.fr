47b48b2b3b04749e434af6b8c7390bd9e4fad4c8
Antoine Augusti <antoine@crowdcube.com>
2015-09-06 15:18:12 +0200

    CSS tweaks for mobile layout of open source projects

--- app/views/projects/singleProject.blade.php
+++ app/views/projects/singleProject.blade.php
@@ -1,6 +1,14 @@
+<?php
+
+$class = '';
+// Extra height if we have several technologies
+if (count($project['technologies']) > 3) {
+    $class = 'extra-height';
+}
+?>
 <div class="col-md-6">
     <a href="{{ $project['url'] }}">
-        <div class="open-source-item">
+        <div class="open-source-item {{ $class }}">
         	<span class="title">{{ $project['title'] }}</span>
             <div class="description">
                 {{ $project['description'] }}
