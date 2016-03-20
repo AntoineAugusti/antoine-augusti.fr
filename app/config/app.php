dfe1e3e352f1f96d8db3bebcda6da154b14d3474
Antoine Augusti <antoine.augusti@gmail.com>
2016-03-20 10:54:42 +0100

    Correct website URL

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://www.antoine-augusti.fr',
+	'url' => 'https://www.antoine-augusti.fr',
 
 	/*
 	|--------------------------------------------------------------------------
